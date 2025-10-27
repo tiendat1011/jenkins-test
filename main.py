e50815d65022c6a64ac8564efb9a8c89a3e65dfe
tiendat1011 <tiendat9tc@gmail.com>
2025-10-27 16:28:17 +0700

    h

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
+import os
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello FastAPI!"}
+    ENVIRONMENT = os.getenv('ENVIRONMENT')
+    DB_HOST = os.getenv('DB_HOST')
+    DB_USERNAME = os.getenv('DB_USERNAME')
+    DB_PASSWORD = os.getenv('DB_PASSWORD')
+    DB_NAME = os.getenv('DB_NAME')
+    DB_PORT = os.getenv('DB_PORT')
+
+    return {"message": f"Hello FastAPI! I'm from env {ENVIRONMENT}"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str | None = None):
